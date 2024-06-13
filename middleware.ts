d9edd3005ce43e775ff526b45fabc9c16d6a0e4a
Alex Alfred <alex.alfred494@gmail.com>
2024-06-13 16:15:42 -0500

    try to clean up webhooks issue AGAIN

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,17 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-// Make sure that the `/api/webhooks/(.*)` route is not protected here
-export default clerkMiddleware()
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/api/webhooks(.*)",
+]);
+
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) {
+    auth().protect();
+  }
+});
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
